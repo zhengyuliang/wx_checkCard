08bc3c4ae9de735f914bbb0c373434fda6d658c9
zhengyuliang <yuliang.zheng@esmart365.com>
2018-10-30 17:30:31 +0800

    核销内容

---
+++ component/modal/modal.js
@@ -0,0 +1,30 @@
+// component/modal/modal.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    title: {
+      type: String,
+      value: ''|| '标题'
+    },
+    titleShow: {
+      type: Boolean,
+      value: true
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
