2f6b11afe5dcd64d9ca0ae628f9e0c7e8ec227fa
zhengyuliang <yuliang.zheng@esmart365.com>
2019-02-25 11:20:16 +0800

    昆仑好客

---
+++ pages/login/login.js
@@ -0,0 +1,113 @@
+// pages/login/login.js
+let app = getApp();
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    // 查看是否授权
+    wx.getSetting({
+      success: function (res) {
+        if (res.authSetting['scope.userInfo']) {
+          wx.navigateTo({
+            url: '../index_data/index_data'
+          })
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+          wx.getUserInfo({
+            success: function (res) {
+              // console.log(res.userInfo)
+              wx.setStorage({
+                key: 'userInfo',
+                data: res.userInfo
+              });
+            }
+          })
+        
+        }
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  bindGetUserInfo: function (e) {
+    let _this = this;
+    if (e.detail.errMsg === 'getUserInfo:ok') {
+      app.globalData.userInfo = e.detail.userInfo;
+      wx.setStorage({
+        key: 'userInfo',
+        data: e.detail.userInfo
+      });
+      wx.navigateTo({
+        url: '../index_data/index_data'
+      })
+      // 返回上一页
+    }
+  },
+  getOpenId:function(){
+    let _that = this;
+    wx.login({
+      success: function(res){
+        console.log("res>>>>",res);
+      },
+      fail:function(err){
+        console.log("err>>>>",err);
+      }
+    })
+  }
+})
